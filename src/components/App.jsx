4f97626f9cfa4c8da88ae8961db03fa642ca9be2
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-04-29 16:56:10 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
 import { Profile } from "./Profile/Profile";
+import user from "user.json";
 
-export const App = () => {
+export const App = ({username, tag, location, avatar, followers, views, likes}) => {
   return (
-    <div>
-      <Profile />
-    </div>
+    <>
+      <Profile 
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    followers={user.stats.followers}
+    views={user.stats.views}
+    likes={user.stats.likes}
+    />
+    </>
   );
 };
