d93f510ac765dd42983b1575a79bce85fd0580a9
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-04-29 20:12:02 +0300

    Update FriendsList.jsx

--- src/components/FriendList/FriendsList/FriendsList.jsx
+++ src/components/FriendList/FriendsList/FriendsList.jsx
@@ -1,6 +1,4 @@
-//import propTypes from 'prop-types'; 
 import css from "./FriendsList.module.css";
-//import friends from "friends.json";
 import { FriendsItem } from '../FriendsItem/FriendsItem';
 
 export const FriendsList = ({friends}) => {
@@ -11,14 +9,3 @@ return (
 )
 }
 
-// FriendsList.propTypes = {
-// friends: propTypes.arrayOf(
-//     propTypes.shape(
-//         {
-//         avatar: propTypes.string,
-//         name: propTypes.string,
-//         isOnline: propTypes.bool,
-//      id: propTypes.number,
-//     }
-// ) )
-// }
