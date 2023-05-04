9ae9c44eb888923d0c78b5a1a522c9e70e1831c5
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-04 15:04:15 +0300

    Update FriendsList.jsx

--- src/components/FriendList/FriendsList.jsx
+++ src/components/FriendList/FriendsList.jsx
@@ -7,22 +7,20 @@ return (
       <ul className={css.friend_list}>
         {friends.map((friend) => {
           return (
-          <FriendListItem key={friend.id} friend={friend}/>
-         
+            <FriendListItem key={friend.id} friend={friend}/>
           )
-         
-})}
+        })}
     </ul> 
 )
 }
 
 FriendsList.propTypes = {
-    friends: propTypes.arrayOf( propTypes.shape (
-      {
+    friends: propTypes.arrayOf( 
+      propTypes.shape ({
         avatar: propTypes.string,
         name: propTypes.string,
         isOnline: propTypes.bool,
         id: propTypes.number,
     }
     )),
-    }
+  }
